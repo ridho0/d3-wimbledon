016194d12e8772327b2cc24093df26617b57a02a
ridho0 <ridho230994@gmail.com>
2017-06-19 14:57:02 +0700

    save dulu

--- script.js
+++ script.js
@@ -15,12 +15,38 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  d3.tsv('afcw-results.tsv', (rows) => {
+    let arrGoal = []
+    rows.map( row => {
+      row.GoalsScored = +row.GoalsScored
+      arrGoal.push(row.GoalsScored)
+    })
+    redraw(arrGoal)
+  })
 }
-
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, height])
 
+  svg.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('fill', 'teal')
+    .attr('x', (d, i) => {
+      return i * 22
+    })
+    .attr('y', (d) => {
+      
+      return 300 - yScale(d)
+    })
+    .attr('width', 20)
+    .attr('height', (d) => {
+      return yScale(d)
+    })
 }
 
 reload()
